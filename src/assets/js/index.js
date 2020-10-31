8ce1ee18b23eaa4bb12145f292c1593fa7fb42f9
Luan <luan.sergio@novahaus.com.br>
2020-10-30 22:39:22 -0300

    working on picross style

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -5,17 +5,14 @@ import createPicross from './modules/picross/creation/createPicross';
 import preventContextMenu from './utils/preventContextMenu'
 import definePicrossSize from './modules/picross/creation/definePicrossSize';
 import generateNewPicross from './modules/picrossSettings/generateNewPicross';
-import bottomClouds from './modules/animations/bottomClouds'
 
-bottomClouds();
+let matrix = createMatrix(5, 5);
+console.log(matrix);
 
-// let matrix = createMatrix(5, 5);
-// console.log(matrix);
+alocatePicrossValues(5, 5, matrix, 5);
 
-// alocatePicrossValues(5, 5, matrix, 5);
+const { width, height } = definePicrossSize();
 
-// const {width, height} = definePicrossSize();
-
-// createPicross(width, height, matrix);
-// generateNewPicross(width, height);
-// preventContextMenu();
+createPicross(width, height, matrix);
+generateNewPicross(width, height);
+preventContextMenu();
--- src/assets/js/modules/picross/creation/createPicross.js
+++ src/assets/js/modules/picross/creation/createPicross.js
@@ -12,25 +12,26 @@ export default function createPicross(width, height, matrix) {
   let row = picross.insertRow();
   let header = document.createElement("TH");
   row.appendChild(header);
-  for ( let i = 0; i < width; i++) {
+  for (let i = 0; i < width; i++) {
     header = document.createElement("TH");
     header.classList.add('picross__numbers');
     row.appendChild(header);
-    header.dataset.numbersY= `${i}`;
+    header.dataset.numbersY = `${i}`;
   }
   for (let i = 0; i < height; i++) {
     header = document.createElement("TH");
     header.classList.add('picross__numbers');
-    header.dataset.numbersX= `${i}`;
+    header.dataset.numbersX = `${i}`;
     let row = picross.insertRow();
     row.appendChild(header);
-      for (let j = 0; j < width; j++) {
-        let cell = row.insertCell();
-        cell.dataset.picrossSquare = '';
-        cell.dataset.x = `${j}`;
-        cell.dataset.y = `${i}`;
-        cell.className = 'picross__square';
-      }
+    row.classList.add('picross__row');
+    for (let j = 0; j < width; j++) {
+      let cell = row.insertCell();
+      cell.dataset.picrossSquare = '';
+      cell.dataset.x = `${j}`;
+      cell.dataset.y = `${i}`;
+      cell.className = 'picross__square';
+    }
   }
 
   printPicrossNumbers(width, height, "row", horizontalNumbersArray);
