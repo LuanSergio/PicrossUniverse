de97f11db5df84dbfc9d7c5fc0c5298d9c61897a
Luan Sergio <luansergiomattos@gmail.com>
2021-01-15 01:40:22 -0300

    working on picross style

--- src/assets/js/modules/picross/creation/definePicrossSize.js
+++ src/assets/js/modules/picross/creation/definePicrossSize.js
@@ -1,10 +1,12 @@
 export default function definePicrossSize() {
   const x = document.querySelector('[data-select-width]');
-  const width = parseInt(x.options[x.selectedIndex].value);
+  const width = parseInt(x.value);
+  // const width = parseInt(x.options[x.selectedIndex].value);
 
   const y = document.querySelector('[data-select-height]');
-  const height = parseInt(y.options[y.selectedIndex].value);
-  
+  const height = parseInt(y.value);
+  // const height = parseInt(y.options[y.selectedIndex].value);
+
   return {
     width,
     height
