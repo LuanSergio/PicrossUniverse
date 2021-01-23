321013e15dd197d4993ecb95626830a76902fd05
Luan Sergio <luansergiomattos@gmail.com>
2021-01-23 03:21:32 -0300

    fix: stop confetti animation when modal is closed

--- src/assets/js/modules/modal/closeModalWhenButtonIsPressed.js
+++ src/assets/js/modules/modal/closeModalWhenButtonIsPressed.js
@@ -1,9 +1,11 @@
 import toogleModal from './toggleModal'
+import confetti from '../../utils/confetti';
 
 export default function closeModalWhenButtonIsPressed() {
   const closeButton = document.querySelector('[data-modal-close]');
 
   closeButton.addEventListener('click', () => {
+    confetti.stop();
     toogleModal();
   });
 }
--- src/assets/js/modules/modal/closeModalWhenOverlayIsClicked.js
+++ src/assets/js/modules/modal/closeModalWhenOverlayIsClicked.js
@@ -1,9 +1,11 @@
 import toogleModal from './toggleModal'
+import confetti from '../../utils/confetti';
 
 export default function closeModalWhenOverlayIsClicked() {
   const overlay = document.querySelector('[data-modal-overlay]');
 
   overlay.addEventListener('click', () => {
+    confetti.stop();
     toogleModal();
   });
 }
