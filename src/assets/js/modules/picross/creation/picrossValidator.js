31bb57a9442ea0a19c0292946fce1cf26dec13b1
Luan Sergio <luansergiomattos@gmail.com>
2021-01-23 03:10:46 -0300

    feat: added win conffeti.js animation

--- src/assets/js/modules/picross/creation/picrossValidator.js
+++ src/assets/js/modules/picross/creation/picrossValidator.js
@@ -1,15 +1,17 @@
 import getPicrossValues from './getPicrossValues'
 import compareMatrices from '../../../utils/compareMatrices'
 import toggleModal from '../../modal/toggleModal'
+import confetti from '../../../utils/confetti.js'
 
 export default function picrossValidator(columns, rows, matrix) {
   const picross = document.querySelector('[data-picross]');
   let picrossValues = [];
-
+  
+  console.log('confetti', confetti)
   picross.addEventListener('click', function () {
     picrossValues = getPicrossValues(columns, rows);
-
     if (compareMatrices(columns, rows, matrix, picrossValues)) {
+      confetti.start();
       toggleModal(columns, rows);
     }
   });
