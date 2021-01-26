2e211d866974d5fa0cc8f31da61bbdf7142b347c
Luan Sergio <luansergiomattos@gmail.com>
2021-01-26 00:21:35 -0300

    feat: added timer into changePicrossClassBasedOnSize

--- src/assets/js/modules/picross/creation/changePicrossClassBasedOnSize.js
+++ src/assets/js/modules/picross/creation/changePicrossClassBasedOnSize.js
@@ -1,16 +1,21 @@
 export default function changePicrossClassBasedOnSize(width, height) {
   const squares = document.querySelectorAll('[data-picross-square]');
   const valuesHolder = document.querySelectorAll('[data-numbers-x],[data-numbers-y]');
+  const timerValues = document.querySelectorAll('[data-timer-hours],[data-timer-minutes],[data-timer-seconds]');
 
   const biggerValue = width > height ? width : height;
+  
+  if(biggerValue > 5) {
+    squares.forEach(square => {
+      square.classList.add(`picross__square-size--${biggerValue}`);
+    });
 
-  console.log('height', height);
-  squares.forEach(square => {
-    square.classList.add(`picross__square-size-${biggerValue}`)
-  });
+    valuesHolder.forEach(value => {
+      value.classList.add(`picross__values-size--${biggerValue}`);
+    });
 
-  valuesHolder.forEach(value => {
-    value.classList.add(`picross__values-size-${biggerValue}`)
-  });
-
+    timerValues.forEach(value => {
+      value.classList.add(`picross__timer--${biggerValue}`);
+    });
+  }
 }
