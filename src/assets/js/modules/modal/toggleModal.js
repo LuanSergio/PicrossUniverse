443fc7877d13ac164be057af3cb1d12df3744a85
Luan Sergio <luansergiomattos@gmail.com>
2021-02-03 01:01:51 -0300

    feat: added aria hidden to modal

--- src/assets/js/modules/modal/toggleModal.js
+++ src/assets/js/modules/modal/toggleModal.js
@@ -4,6 +4,13 @@ import addTimeToModal from './addTimeToModal';
 export default function toggleModal(width, height) {
   const modal = document.querySelector('[data-modal]');
   addTimeToModal();
+
+  if (modal.getAttribute('aria-hidden') == 'true') {
+    modal.setAttribute('aria-hidden', 'false');
+  } else if (modal.getAttribute('aria-hidden') == 'false') {
+    modal.setAttribute('aria-hidden', 'true');
+  }
+
   addSizeToModal(width, height);
   modal.classList.toggle('hidden');
 }
