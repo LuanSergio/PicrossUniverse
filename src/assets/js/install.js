c72490d42ab4229e44569dbd2f1010b9091651ab
Luan Sergio <luansergiomattos@gmail.com>
2021-02-24 18:37:04 -0300

    refactor: removed auto pwa installPrompt

--- src/assets/js/install.js
+++ src/assets/js/install.js
@@ -2,16 +2,16 @@ let deferredInstallPrompt = null;
 const screenWidth = screen.width;
 const installButton = document.querySelector('[data-install-pwa-button]');
 window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+
 installButton.addEventListener('click', installPWA);
 
-function saveBeforeInstallPromptEvent(evt) {
+function saveBeforeInstallPromptEvent(evt) {;
   deferredInstallPrompt = evt;
-  deferredInstallPrompt.prompt();
 }
 
-
 function installPWA(evt) {
+  installButton.setAttribute('aria-hidden', true);
+  installButton.classList.add('hidden');  
   deferredInstallPrompt.prompt();
-  installButton.srcElement.setAttribute('aria-hidden', true);
-  installButton.classList.add('hidden');
+  console.log('is it installed', navigator.getInstalledRelatedApps());
 }
