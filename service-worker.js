51529bbd1eb43efff63c0eb3341764b722769901
Luan Sergio <luansergiomattos@gmail.com>
2021-03-05 23:34:46 -0300

    fix: changed service worker respondWith to offline.html

--- service-worker.js
+++ service-worker.js
@@ -36,7 +36,7 @@ self.addEventListener('fetch', (evt) => {
       .catch(() => {
         return caches.open(CACHE_NAME)
         .then((cache) => {
-          return cache.match('index.html');
+          return cache.match('offline.html');
         });
     })
   );
