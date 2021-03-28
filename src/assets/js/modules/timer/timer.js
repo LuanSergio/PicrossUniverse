593ce6ee85ed1deb03a24f27506e312d2fdf1903
Luan Sergio <luansergiomattos@gmail.com>
2021-03-28 04:54:18 -0300

    fix: fixed timer minutes numbers when turning into hours

--- src/assets/js/modules/timer/timer.js
+++ src/assets/js/modules/timer/timer.js
@@ -2,23 +2,21 @@ export default function timer() {
   let hours = 0;
   let minutes = 0;
   let seconds = 0;
-  const modal = document.querySelector('[data-modal-winning-screen]');
-  const secondsHolder = document.querySelector('[data-timer-seconds');
-  const minutesHolder = document.querySelector('[data-timer-minutes');
-  const hoursHolder = document.querySelector('[data-timer-hours');
-  const buttonGenerate = document.querySelector('[data-generate-picross]');
-  
+  const modal = document.querySelector("[data-modal-winning-screen]");
+  const secondsHolder = document.querySelector("[data-timer-seconds");
+  const minutesHolder = document.querySelector("[data-timer-minutes");
+  const hoursHolder = document.querySelector("[data-timer-hours");
+  const buttonGenerate = document.querySelector("[data-generate-picross]");
+
   seconds++;
 
   let timer = setInterval(function () {
-
-    if (!(modal.classList.contains('hidden'))) {
+    if (!modal.classList.contains("hidden")) {
       clearInterval(timer);
     }
-    buttonGenerate.addEventListener('click', () => {
+    buttonGenerate.addEventListener("click", () => {
       clearInterval(timer);
-    })
-
+    });
 
     seconds = parseInt(seconds);
     minutes = parseInt(minutes);
@@ -27,35 +25,33 @@ export default function timer() {
     if (seconds == 60) {
       minutes = minutes + 1;
       if (minutes < 10) {
-        minutes = '0' + minutes;
+        minutes = "0" + minutes;
       }
       minutesHolder.innerHTML = ":" + minutes + ":";
       seconds = 0;
     }
 
     if (minutes == 60) {
+      minutes = "0" + 0;
       hours = hours + 1;
       if (hours < 10) {
-        hours = '0' + hours;
+        hours = "0" + hours;
       }
+      minutesHolder.innerHTML = ":" + minutes + ":";
       hoursHolder.innerHTML = hours;
-      minutes = 0;
       seconds = 0;
     }
     if (seconds < 10) {
-      seconds = '0' + seconds;
+      seconds = "0" + seconds;
     }
     secondsHolder.innerHTML = seconds;
-    
-    if (!(modal.classList.contains('hidden'))) {
-      secondsHolder.innerHTML = '00';
-      minutesHolder.innerHTML = ":" + '00' + ":";
-      hoursHolder.innerHTML = '00';
+
+    if (!modal.classList.contains("hidden")) {
+      secondsHolder.innerHTML = "00";
+      minutesHolder.innerHTML = ":" + "00" + ":";
+      hoursHolder.innerHTML = "00";
     }
 
     seconds++;
-
   }, 1000);
-
 }
-
